ac1d58db9da08b3a4dd1e333673f4f378b70894e
ParikshitShetty <parikshitshetty66@gmail.com>
2024-06-21 00:14:35 +0530

    dockerized need to volume mapping properly

--- server/redisClient.js
+++ server/redisClient.js
@@ -3,7 +3,8 @@ const redis  = require('redis')
 // Create Redis Client
 const redisClient = redis.createClient({
     socket:{
-        host: '127.0.0.1',
+        // host: '127.0.0.1',
+        host: 'redis-server',
         port : 6379,
     },
 });
